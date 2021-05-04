512e952430ae705a998a1b86e2a969501b16c978
Madi <63842217+Madi0110@users.noreply.github.com>
2021-05-05 00:15:59 +0600

    Add files via upload

---
+++ bus.dart
@@ -0,0 +1,21 @@
+class Bus {
+  String name;
+  String from;
+  String to;
+  DateTime fromDateTime;
+  DateTime toDateTime;
+  String seats;
+  String carNumber;
+  String icon;
+
+  Bus({
+    this.name,
+    this.from,
+    this.to,
+    this.fromDateTime,
+    this.toDateTime,
+    this.seats,
+    this.carNumber,
+    this.icon,
+  });
+}
